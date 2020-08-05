961dfa6777426e92f536edfcc0c476c594541db3
hsim <hsim@daou.co.kr>
2020-08-05 16:42:12 +0900

    security resolver super class check bug fix

--- lib/src/main/java/hsim/simple/security/resolver/SimpleUserDetailsResolver.java
+++ lib/src/main/java/hsim/simple/security/resolver/SimpleUserDetailsResolver.java
@@ -21,7 +21,7 @@ public class SimpleUserDetailsResolver implements HandlerMethodArgumentResolver 
 
     @Override
     public boolean supportsParameter(MethodParameter methodParameter) {
-        return methodParameter.getParameterType().getSuperclass().equals(SimpleUserDetails.class);
+        return methodParameter.getParameterType().isAssignableFrom(SimpleUserDetails.class);
     }
 
     @Override
